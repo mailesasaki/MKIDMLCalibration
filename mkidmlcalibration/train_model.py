4541257d3a489712f075e4c9c7db7a20f9485740
mailesasaki <69178214+mailesasaki@users.noreply.github.com>
2021-08-28 18:50:36 -0700

    Update train_model.py

--- mkidmlcalibration/train_model.py
+++ mkidmlcalibration/train_model.py
@@ -5,6 +5,7 @@ Trains the model when given an mlDict file
 import wpsnnmkidkal2
 import mkidcore.config as config
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='ML model trainer')
 parser.add_argument('mlDict', help='yaml file containing mlDict information')
