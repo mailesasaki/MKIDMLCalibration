ff3c3804027a3eeacf48e6fced55c899b9322830
mailesasaki <69178214+mailesasaki@users.noreply.github.com>
2021-08-31 20:42:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    name="mkidresonatorkal",
+    name="mkidmlcalibration",
     version="0.1",
     author="MazinLab",
     author_email="mazinlab@ucsb.edu",
     description="An UVOIR MKID ML Calibration Package",
     long_description_content_type="text/markdown",
-    url="https://github.com/mailesasaki/mkidresonatorkal",
-    scripts=['mkidresonatorkal/findResonatorsWPS.py'],
+    url="https://github.com/mailesasaki/mkidmlcalibration",
+    scripts=['mkidmlcalibration/findResonatorsWPS.py'],
     packages=setuptools.find_packages(),
 )
