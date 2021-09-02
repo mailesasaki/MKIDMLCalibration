2eb07c65848978217f0a66cec3687762539ccc61
mailesasaki <69178214+mailesasaki@users.noreply.github.com>
2021-09-01 23:52:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     description="An UVOIR MKID ML Calibration Package",
     long_description_content_type="text/markdown",
     url="https://github.com/mailesasaki/mkidmlcalibration",
-    scripts=['mkidmlcalibration/findResonatorsWPS.py'],
+    scripts=['mkidmlcalibration/findResonatorsWPS.py',
+             'mkidmlcalibration/train_model.py'],
     packages=setuptools.find_packages(),
 )
