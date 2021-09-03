05dedf0652ab78fd23a24d577ff70be5a22987dd
mailesasaki <69178214+mailesasaki@users.noreply.github.com>
2021-09-03 14:02:53 -0700

    Update findResonatorsWPS.py

--- mkidmlcalibration/findResonatorsWPS.py
+++ mkidmlcalibration/findResonatorsWPS.py
@@ -19,9 +19,9 @@ import logging
 import matplotlib.pyplot as plt
 import skimage.feature as skf
 import mkidcore.sweepdata as sd
-import mkidresonatorkal.tools as mlt
+import mkidmlcalibration.tools as mlt
 from mkidcore.corelog import getLogger
-from mkidresonatorkal.wpsnnmkidkal2 import N_CLASSES
+from mkidmlcalibration.wpsnnmkidkal2 import N_CLASSES
 import mkidcore.instruments as inst
 
 N_RES_PER_BOARD = 1024
