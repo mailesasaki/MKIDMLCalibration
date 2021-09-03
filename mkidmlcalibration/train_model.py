dbbbd6b8697140768f0decb00fc45f38c8c8d754
mailesasaki <69178214+mailesasaki@users.noreply.github.com>
2021-09-03 13:53:49 -0700

    Update train_model.py

--- mkidmlcalibration/train_model.py
+++ mkidmlcalibration/train_model.py
@@ -9,7 +9,8 @@ import os
 
 parser = argparse.ArgumentParser(description='ML model trainer')
 parser.add_argument('mlDict', help='yaml file containing mlDict information')
-parser.add_argument('model_name', help='destination of ML model')
+parser.add_argument('model_name', help='destination + name of ML model folder')
+parser.add_argument('plot_destination', help='destinations of accuracy + loss plots')
 args=parser.parse_args()
 
 if os.path.isfile(args.mlDict) == False:
