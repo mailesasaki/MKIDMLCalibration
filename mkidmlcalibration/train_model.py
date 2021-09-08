689fb56393e77fd93b471127bcec99a9aa37d2a1
mailesasaki <69178214+mailesasaki@users.noreply.github.com>
2021-09-07 22:52:43 -0700

    Update train_model.py

--- mkidmlcalibration/train_model.py
+++ mkidmlcalibration/train_model.py
@@ -9,15 +9,13 @@ import os
 
 parser = argparse.ArgumentParser(description='ML model trainer')
 parser.add_argument('mlDict', help='yaml file containing mlDict information')
-parser.add_argument('model_name', help='destination + name of ML model folder')
 args=parser.parse_args()
 
 if os.path.isfile(args.mlDict) == False:
         raise Exception('mlDict file does not exist')
     
 mlDict_file = args.mlDict
-model_name = args.model_name
 mlDict = config.load(mlDict_file)
 
-trial1 = wpsnn.WPSNeuralNet(mlDict, model_name, mlDict_file)
+trial1 = wpsnn.WPSNeuralNet(mlDict, mlDict_file)
 trial1.initializeAndTrainModel()
